b51fdf0fb3cc67a694011ee033801471f4576798
Simon B <simon.blecot@wifirst.fr>
2025-11-17 17:09:11 +0100

    Start CSS header

--- DashBoard/src/pages/Dashboard.jsx
+++ DashBoard/src/pages/Dashboard.jsx
@@ -3,7 +3,11 @@ export default function Dashboard() {
     return (
         <div>
            <header>
-
+                <h1>Mon Tabelau de Bord</h1>
+                <div className="weather-date">
+                    <p>Meteo</p>
+                    <p>Date</p>
+                </div>
            </header>
         </div>
     )
