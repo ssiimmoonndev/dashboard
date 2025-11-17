601448fb51d1665138b79d5a9803c882130448bb
Simon B <simon.blecot@wifirst.fr>
2025-11-17 16:03:15 +0100

    Layout-background-blue

--- DashBoard/src/pages/Dashboard.jsx
+++ DashBoard/src/pages/Dashboard.jsx
@@ -3,11 +3,7 @@ export default function Dashboard() {
     return (
         <div>
            <header>
-                <h1>Mon Tabelau de Bord</h1>
-                <div className="meteo-heure">
-                    <h3>Meteo</h3>
-                    <h3>Date</h3>
-                </div>
+
            </header>
         </div>
     )
