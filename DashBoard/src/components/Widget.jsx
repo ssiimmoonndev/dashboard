4f77a4941d5fcad21e003f721d151c978f72276c
Simon B <simon.blecot@wifirst.fr>
2025-11-17 11:31:51 +0100

    Debut du projet

--- DashBoard/src/components/Widget.jsx
+++
@@ -1,8 +0,0 @@
-export default function Widget(props) {
-    return (
-        <div className="card">
-            <h2>{props.title}</h2>
-            <p>{props.children}</p>
-        </div>
-    )
-}
--- DashBoard/src/pages/Dashboard.jsx
+++ DashBoard/src/pages/Dashboard.jsx
@@ -4,14 +4,6 @@ export default function Dashboard() {
     return (
         <div>
             <h1>Mon DashBoard</h1>
-            <div className="container-widget">
-            <Widget title="Météo" />
-            <Widget title="Horloge" />
-            <Widget title="Bourse" />
-            <Widget title="Crypto" />
-            <Widget title="Résultats sportifs" />
-            <Widget title="Planning" />
-            </div>
         </div>
     )
 }
