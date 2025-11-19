bfba657519526c4196085311880321087622e53e
Simon B <simon.blecot@wifirst.fr>
2025-11-19 15:04:57 +0100

    component Header

--- DashBoard/src/components/Header.jsx
+++ DashBoard/src/components/Header.jsx
@@ -1,2 +1,15 @@
 import React from 'react';
 
+function Header() {
+    return (
+        <header>
+            <h1>Mon Tableau de Bord</h1>
+            <div className="weather-date">
+                <div>Météo</div>
+                <div>Date</div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
