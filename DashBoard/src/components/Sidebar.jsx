a1b8615d5bd863a69cf13fb758375f72643cfedc
Simon B <simon.blecot@wifirst.fr>
2025-11-19 15:06:27 +0100

    component Sidebar

--- DashBoard/src/components/Sidebar.jsx
+++ DashBoard/src/components/Sidebar.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <button className="button-sidebar">Planning</button>
+            <button className="button-sidebar">Crypto / Bourse</button>
+            <button className="button-sidebar">Résultats sportifs</button>
+            <button className="button-sidebar">À propos</button>
+        </div>
+    );
+}
+
+export default Sidebar;
